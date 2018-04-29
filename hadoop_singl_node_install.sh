3b8e90ba3464a2442933832a983b94e644a94b99
smithagarwal <smith.agarwal1990@gmail.com>
2018-04-29 19:25:11 +0200

    Updated the script v0.2

--- hadoop_singl_node_install.sh
+++ hadoop_singl_node_install.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 echo -n "Enter the new 'GroupName' for new Hadoop user : "
@@ -6,7 +5,6 @@ read hdGroup
 echo -n "Enter the new 'UserName' for Hadoop: "
 read hdUserName
 
-:<<comment
 echo "...................Updating your System.................."
 
 sudo apt-get update
@@ -71,7 +69,6 @@ sudo -u $hdUserName mkdir -p /home/$hdUserName/hadoop/hadoop_store/hdfs/datanode
 sudo -u $hdUserName chown -R $hdUserName:$hdGroup /home/$hdUserName/hadoop/hadoop_store
 
 #Giving permission to write the .bashrc, hadoop-env.sh core-site.xml, mapred-site.xml, hdfs-site.xml, yarn-site.xml.
-comment
 
 sudo -u $hdUserName chmod o+w /home/$hdUserName/.bashrc
 sudo -u $hdUserName chmod o+w /home/$hdUserName/hadoop/etc/hadoop/hadoop-env.sh
@@ -82,21 +79,23 @@ sudo -u $hdUserName chmod o+w /home/$hdUserName/hadoop/etc/hadoop/yarn-site.xml
 
 #hadoop-env.sh
 
-sudo sed -i "s|\${JAVA_HOME}|$java_home|g" /home/$hdUserName/hadoop/etc/hadoop/hadoop-env.sh
+
 
 echo -e '\n\n #Hadoop Variable START \n export HADOOP_HOME=/home/'$hdUserName'/hadoop \n export HADOOP_INSTALL=$HADOOP_HOME \n export HADOOP_MAPRED_HOME=$HADOOP_HOME \n export HADOOP_COMMON_HOME=$HADOOP_HOME \n export HADOOP_HDFS_HOME=$HADOOP_HOME \n export YARN_HOME=$HADOOP_HOME \n export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native \n export PATH=$PATH:$HADOOP_HOME/sbin/:$HADOOP_HOME/bin \n #Hadoop Variable END\n\n' >> /home/$hdUserName/.bashrc
 
 source /home/$hdUserName/.bashrc
 
 #core-site.xml
-sudo sed -i '/<configuration>/a <property>\n\t\t<name>hadoop.tmp.dir</name>\n\t\t<value>/home/hduser/hadoop/app/hadoop/tmp</value>\n</property>\n<property>\n\t\t<name>fs.default.name</name>\n\t\t<value>hdfs://localhost:54310</value>\n</property>' /home/$hdUserName/hadoop/etc/hadoop/core-site.xml
+sudo sed -i '/<configuration>/a <property>\n\t\t<name>fs.default.name</name>\n\t\t<value>hdfs://master:9000</value>\n</property>' /home/$hdUserName/hadoop/etc/hadoop/core-site.xml
 
 #mapred-site.xml
 sudo sed -i '/<configuration>/a <property>\n\t\t <name>mapreduce.framework.name</name>\n\t\t <value>yarn</value>\n</property>' /home/$hdUserName/hadoop/etc/hadoop/mapred-site.xml
 
 #Yarn-site.xml
-sudo sed -i '/<configuration>/a <property>\n\t\t<name>yarn.nodemanager.aux-services</name>\n\t\t<value>mapreduce_shuffle</value>\n</property>' /home/$hdUserName/hadoop/etc/hadoop/yarn-site.xml
+sudo sed -i '/<configuration>/a <property>\n\t\t<name>yarn.nodemanager.aux-services</name>\n\t\t<value>mapreduce_shuffle</value>\n</property>\n<property>\n\t\t<name>yarn.nodemanager.auxservices.mapreduce.shuffle.class</name>\n\t\t<value>org.apache.hadoop.mapred.ShuffleHandler</value>\n</property>' /home/$hdUserName/hadoop/etc/hadoop/yarn-site.xml
 
+#hdfs-site.xml
+sudo sed -i '/<configuration>/a <property>\n\t\t<name>dfs.replication</name>\n\t\t<value>2</value>\n</property>\n<property>\n\t\t<name>dfs.permissions</name>\n\t\t<value>false</value>\n</property>\n<property>\n\t\t<name>dfs.namenode.name.dir</name>\n\t\t<value>/home/hduser/hadoop/hadoop_store/hdfs/namenode</value>\n</property>\n<property>\n\t\t<name>dfs.datanode.data.dir</name>\n\t\t<value>/home/hduser/hadoop/hadoop_store/hdfs/datanode</value>\n</property>' /home/$hdUserName/hadoop/etc/hadoop/hdfs-site.xml
 
 #revoking write permission for .bashrc, hadoop-env.sh core-site.xml, mapred-site.xml, hdfs-site.xml, yarn-site.xml files. 
 sudo -u $hdUserName chmod o-w /home/$hdUserName/.bashrc
@@ -105,6 +104,9 @@ sudo -u $hdUserName chmod o-w /home/$hdUserName/hadoop/etc/hadoop/core-site.xml
 sudo -u $hdUserName chmod o-w /home/$hdUserName/hadoop/etc/hadoop/mapred-site.xml
 sudo -u $hdUserName chmod o-w /home/$hdUserName/hadoop/etc/hadoop/hdfs-site.xml
 sudo -u $hdUserName chmod o-w /home/$hdUserName/hadoop/etc/hadoop/yarn-site.xml
+
+#Opening the port in iptables
+sudo -u $hdUserName sudo /hadoop/sbin/iptables -A INPUT -p tcp --dport 50070 -j ACCEPT
 
 echo "--------------------HADOOP DIRECTORY------------------------- "
 sudo ls /home/$hdUserName/hadoop/
