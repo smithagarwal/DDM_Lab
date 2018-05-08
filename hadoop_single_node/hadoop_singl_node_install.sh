c6d624d942b0508847f8575215af80b65555cb11
smithagarwal <smith.agarwal1990@gmail.com>
2018-05-08 10:35:15 +0200

    Included the export command to set java_home

--- hadoop_single_node/hadoop_singl_node_install.sh
+++ hadoop_single_node/hadoop_singl_node_install.sh
@@ -19,6 +19,7 @@ else
 	echo "-----------------Removing older version of Java and installing default JDK of Ubuntu--------------"
 	sudo apt-get autoremove java-common
 	sudo apt-get install default-jdk
+	export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/
 fi 
 
 #Getting JAVA_HOME value and storing in java_home variable
